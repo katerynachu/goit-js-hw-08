afd7b4f5e6516fa998b9eba9d60a5e6becbb094f
Kateryna <k.churuta@gmail.com>
2023-09-10 14:54:30 +0100

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(player);
+if (player) {
+  player.on('timeupdate', throttle(saveCurrentTime, 1000));
+}
+
+function saveCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveDataToLocalStorage = () => {
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+const fillFormFieldsFromLocalStorage = () => {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  emailInput.value = data.email || '';
+  messageInput.value = data.message || '';
+};
+
+emailInput.addEventListener(
+  'input',
+  throttle(() => {
+    saveDataToLocalStorage();
+  }, 500)
+);
+
+messageInput.addEventListener(
+  'input',
+  throttle(() => {
+    saveDataToLocalStorage();
+  }, 500)
+);
+
+
+window.addEventListener('load', () => {
+  fillFormFieldsFromLocalStorage();
+});
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(data);
+
+
+  localStorage.removeItem('feedback-form-state');
+
+
+  emailInput.value = '';
+  messageInput.value = '';
+});
 
